2c0b6b3f35b73d30b9d8f974281e4ad767bae793
oldentomato <ender1412@naver.com>
2025-05-28 13:28:37 +0900

    :bug: fix: 코드 rollback

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,11 +1,10 @@
 // router 설정해두는 파일입니다.
 
 import { createBrowserRouter } from "react-router";
-import QuestionComponent from "@widgets/QuestionComponent/QuestionComponent"
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <QuestionComponent />,
+        element: <p>test</p>,
     },
 ]);
