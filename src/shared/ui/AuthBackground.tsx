095a01a4a4baa8c3ac7d74adc46b76bcf243ac72
minju3212 <smj3mjs@google.com>
2025-05-29 14:57:11 +0900

    :sparkles: feat: authbackground 추가

---
+++ src/shared/ui/AuthBackground.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AuthBackground: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: '#fffcee',
+        minHeight: '100vh',
+        width: '100%',
+      }}
+    >
+    </div>
+  );
+};
+
+export default AuthBackground;
