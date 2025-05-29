5548a9e2b6e93a91597e43367e0b75b2a0b52398
raniyaya <pinik6586@gamil.com>
2025-05-29 17:34:56 +0900

    :sparkles: feat: 사원조회페이지완성(loginform네비게이션수정)

--- src/widgets/EmRegister/EmRegister.tsx
+++ src/widgets/EmRegister/EmRegister.tsx
@@ -4,9 +4,11 @@ import logoImage from "@shared/assets/icon/logo.png";
 import theme from "@app/styles/theme";
 import { getEmployeeInfo } from "@shared/Apis/emregister";
 import type { EmployeeInfo } from "@shared/Apis/emregister";
+import { useNavigate } from "react-router-dom";
 
 const EmRegister = () => {
     const [employee, setEmployee] = useState<EmployeeInfo>({ employeeNumber: "" });
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,10 +20,11 @@ const EmRegister = () => {
 
     const handleSearch = () => {
         if (!employee.employeeNumber) {
-            alert("사원번호를 입력해주세요.");
+            alert("사원이 아닙니다.");
             return;
         }
-        alert(`사원번호 ${employee.employeeNumber} 조회 중...`);
+        alert("확인 완료!");
+        navigate("/register"); // ✅ 회원가입 페이지로 이동
     };
 
     return (
@@ -40,8 +43,8 @@ const EmRegister = () => {
                             setEmployee({ ...employee, employeeNumber: e.target.value })
                         }
                     />
+                    <Button onClick={handleSearch}>조회하기</Button>
                 </InputGroup>
-                <Button onClick={handleSearch}>조회하기</Button>
             </Container>
         </PageWrapper>
     );
@@ -56,9 +59,8 @@ const PageWrapper = styled.div`
 `;
 
 const Logo = styled.img`
-    width: 25%;
-    max-width: 100px;
-    margin-bottom: 5%;
+    width: 200%;
+    max-width: 200px;
 `;
 
 const Title = styled.h2`
@@ -68,8 +70,8 @@ const Title = styled.h2`
 `;
 
 const Container = styled.div`
-    width: 57.22%;
-    height: 80.47%;
+    width: 120%;
+    height: 80%;
     background-color: ${theme.yellow.y100};
     display: flex;
     flex-direction: column;
@@ -83,7 +85,7 @@ const Container = styled.div`
 const InputGroup = styled.div`
     display: flex;
     flex-direction: column;
-    width: 80%;
+    width: 120%;
     max-width: 300px;
     margin-bottom: 25px;
     align-items: flex-start;
@@ -108,8 +110,8 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-    width: 80%;
-    max-width: 300px;
+    width: 100%;
+    margin-top: 30px;
     padding: 10px 15px;
     background-color: ${theme.orange.o500};
     color: white;
